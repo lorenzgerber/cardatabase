50a2086b256c3d6c5daf59402fea009fe026227d
Lorenz Gerber <genfys@gmail.com>
2018-12-09 21:57:45 +0800

    createc crud repo

--- src/main/java/com/lorenzgerber/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/lorenzgerber/cardatabase/CardatabaseApplication.java
@@ -1,12 +1,28 @@
 package com.lorenzgerber.cardatabase;
 
+import org.springframework.boot.CommandLineRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CardatabaseApplication {
+	
+	public static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
 
 	public static void main(String[] args) {
+		// After adding this comment the application is restarted		
 		SpringApplication.run(CardatabaseApplication.class, args);
+		logger.info("Ers ma loggn");
 	}
+	
+	@Bean
+    CommandLineRunner runner(){
+      return args -> {
+        // Place your code here
+      };
+    }
+	
 }
