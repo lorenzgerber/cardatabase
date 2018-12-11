69c0d27e25cab841d1ff4057d2bcb287185045c7
Lorenz Gerber <lorenz.gerber@lablicate.com>
2018-12-11 22:39:07 +0800

    prep for H2 in-memory db

--- src/main/java/com/lorenzgerber/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/lorenzgerber/cardatabase/CardatabaseApplication.java
@@ -1,5 +1,6 @@
 package com.lorenzgerber.cardatabase;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,10 +8,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.lorenzgerber.cardatabase.domain.Car;
+import com.lorenzgerber.cardatabase.domain.CarRepository;
+
 @SpringBootApplication
 public class CardatabaseApplication {
 	
 	public static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
+	
+	@Autowired 
+    private CarRepository repository;
 
 	public static void main(String[] args) {
 		// After adding this comment the application is restarted		
@@ -21,7 +28,10 @@ public class CardatabaseApplication {
 	@Bean
     CommandLineRunner runner(){
       return args -> {
-        // Place your code here
+    	// Save demo data to database
+        repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000));
+        repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000));
+        repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000));
       };
     }
 	
