9ffaf61abaed80ba9d61aef67da37e46d40648fa
Lorenz Gerber <lorenzottogerber@gmail.com>
2023-11-14 11:07:54 +0100

    adding back security to backend

--- src/main/java/com/packt/cardatabase/SecurityConfig.java
+++ src/main/java/com/packt/cardatabase/SecurityConfig.java
@@ -2,12 +2,22 @@ package com.packt.cardatabase;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -16,14 +26,14 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 public class SecurityConfig {
 
-//	@Autowired
-//	private UserDetailsService uds;
-//	
-//	@Autowired
-//	private AuthenticationFilter authenticationFilter;
-//	
-//	@Autowired
-//	private AuthEntryPoint exceptionHandler;
+	@Autowired
+	private UserDetailsService uds;
+
+	@Autowired
+	private AuthenticationFilter authenticationFilter;
+
+	@Autowired
+	private AuthEntryPoint exceptionHandler;
 
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
@@ -43,25 +53,27 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf(AbstractHttpConfigurer::disable).cors(cors -> cors.configurationSource(corsConfigurationSource()));
-//		.sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//		.authorizeHttpRequests( request -> request.requestMatchers(HttpMethod.POST, "/login").permitAll().anyRequest().authenticated())
-//		.authenticationProvider(authenticationProvider()).addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
-//		.exceptionHandling((exception) -> exception.authenticationEntryPoint(exceptionHandler));
+		http.csrf(AbstractHttpConfigurer::disable).cors(cors -> cors.configurationSource(corsConfigurationSource()))
+				.sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.POST, "/login").permitAll()
+						.anyRequest().authenticated())
+				.authenticationProvider(authenticationProvider())
+				.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+				.exceptionHandling((exception) -> exception.authenticationEntryPoint(exceptionHandler));
 		return http.build();
 	}
 
-//	@Bean
-//	public AuthenticationProvider authenticationProvider() {
-//		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-//		authenticationProvider.setUserDetailsService(uds);
-//		authenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder());
-//		return authenticationProvider;
-//	}
-//	
-//	@Bean
-//	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-//		return config.getAuthenticationManager();
-//	}
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(uds);
+		authenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder());
+		return authenticationProvider;
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+	}
 
 }
